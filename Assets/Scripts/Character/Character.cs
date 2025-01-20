21e4e825f136b576a2f3e627db8e780071732726
Chiller132196 <2580911814@qq.com>
2025-01-20 18:50:38 +0800

    初始化角色父类

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -4,15 +4,99 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region 角色属性
+    /// <summary>
+    /// 角色力量
+    /// </summary>
+    public int strength;
+
+    /// <summary>
+    /// 角色活力
+    /// </summary>
+    public int vitality;
+
+    /// <summary>
+    /// 角色敏捷
+    /// </summary>
+    public int agility;
+
+    /// <summary>
+    /// 角色灵巧
+    /// </summary>
+    public int dex;
+
+    /// <summary>
+    /// 角色意志
+    /// </summary>
+    public int will;
+
+    /// <summary>
+    /// 角色知识
+    /// </summary>
+    public int knowledge;
+
+    /// <summary>
+    /// 角色智谋
+    /// </summary>
+    public int wisdom;
+
+    /// <summary>
+    /// 角色魔力存量
+    /// </summary>
+    public int storage;
+
+    #endregion
+
+    /// <summary>
+    /// 角色外观
+    /// </summary>
+    public object out_look;
+
+    /// <summary>
+    /// 角色技能
+    /// </summary>
+    public List<object> skill;
+
+    /// <summary>
+    /// 角色血量
+    /// </summary>
+    private double health;
+
+    /// <summary>
+    /// 角色精力
+    /// </summary>
+    private int energy;
+
+    /// <summary>
+    /// 角色魔力
+    /// </summary>
+    private int mana;
+
+    /// <summary>
+    /// 角色机动性
+    /// </summary>
+    private int motility;
+
+    /// <summary>
+    /// 初始化角色二级属性
+    /// </summary>
+    public virtual void Load()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeHealth(int amount)
     {
-        
+        health = health + amount;
+    }
+
+    public void ChangeMana(int amount)
+    {
+        mana = mana + amount;
+    }
+
+    public void ChangeEnergy(int amount)
+    {
+        energy = energy + amount;
     }
 }
